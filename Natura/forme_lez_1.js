526153909a277612b734b2584e0b9bb30b0dcf7d
Adriano Parracciani <cyberparra@users.noreply.github.com>
2022-12-29 17:20:31 +0100

    Create forme_lez_1.js

---
+++ Natura/forme_lez_1.js
@@ -0,0 +1,21 @@
+function setup() {
+  createCanvas(300, 200);
+  background(255);
+  stroke(0);
+  strokeWeight(2);
+  noFill();
+
+  beginShape();
+  vertex(100, 50);
+  vertex(200, 70);
+  vertex(200, 130);
+  vertex(100, 150);  
+  vertex(100, 200);
+  endShape();
+}
+
+function draw() {
+  //background(220);
+
+
+}
